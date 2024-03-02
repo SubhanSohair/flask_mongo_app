40423435dd988e9d2f67f7f930955146fa7c6e35
“Subhan <subhansohair@gmail.com>
2024-03-02 17:23:15 +0500

    init

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+from db import save_review, load_reviews
+
+app = Flask(__name__)
+app.secret_key = "subi"
+
+# Home Page
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+# QR Page
+@app.route("/qr")
+def qr():
+    return render_template("qr.html")
+
+# Review Submitted Page
+@app.route("/review_submitted", methods = ['POST','GET'])
+def review_submitted():
+    data = request.form
+    # print(data['username'])
+    # print(data['message'])
+    save_review(data['username'], data['message'])
+
+    return render_template("review_submitted.html")
+
+# Reviews Page
+@app.route("/reviews")
+def reviews():
+    reviews = load_reviews()  # Assuming you have a function to load reviews from the database
+    return render_template("reviews.html", reviews=reviews)
+
---
+++ db.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+CON_STRING =  os.environ['CON_STRING']
+
+client = MongoClient(CON_STRING)
+
+db = client.get_database('ReviewDB')
+
+reviews_collection = db.get_collection('reviews')
+
+
+# Save Review
+def save_review(username, review):
+    reviews_collection.insert_one({'username': username, 'review': review})
+
+
+def load_reviews():
+    reviews = list(reviews_collection.find())
+    return reviews
+
